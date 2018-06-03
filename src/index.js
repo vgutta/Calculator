3499a620218588cd292b5f5a0ec01542b21947da
vgutta <vineethg@udel.edu>
2018-06-03 19:53:58 -0400

    index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
