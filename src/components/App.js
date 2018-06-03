3da9d89b32cc714877b23b4fb3bfdaa66747d236
vgutta <vineethg@udel.edu>
2018-06-03 15:33:49 -0400

    everything

--- src/components/App.js
+++ src/components/App.js
@@ -25,9 +25,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="component-app">
+          <Display value={this.state.next || this.state.total || "0"} />
+          <ButtonPanel clickHandler={this.handleClick} />
+        </div>
       </div>
     );
   }
